cffd831706e5485a0839ab69f0587d293fa61284
Sameer Shaik <125149832+sameers22@users.noreply.github.com>
2025-03-10 01:44:52 -0700

    backend_draftLF

--- src/pages/LegendFamily.jsx
+++ src/pages/LegendFamily.jsx
@@ -8,7 +8,7 @@ const LegendFamily = () => {
     useEffect(() => {
         const fetchVideos = async () => {
             try {
-                const response = await fetch('https://legendcode.onrender.com/api/scrape-videos');
+                const response = await fetch('http://localhost:5001/api/scrape-videos');
                 if (!response.ok) {
                     throw new Error('Failed to fetch videos');
                 }
